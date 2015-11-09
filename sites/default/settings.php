d46cf2c1e8ce60177a2d6a66f81ac8d8f14fde5a
Kevin Tokheim & Phillip Shannon <student@epicodus.com>
2015-11-09 12:14:31 -0800

    Add user profile and update database.

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -212,7 +212,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'come_along_database',
+      'username' => 'epicodus',
+      'password' => 'epicodus',
+      'host' => '127.0.0.1',
+      'port' => '8889',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -244,7 +258,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = 'A_30qHnp8TPkLTByELRMreUO8KNz5_4jDDjaqqBle4U';
 
 /**
  * Base URL (optional).
